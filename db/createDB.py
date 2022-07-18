cfdb499cb51f70968d53833948db7b7c1dd1043a
Shaharyar Lakhani <shaharyar.lakhani@utexas.edu>
2022-07-18 15:20:29 -0500

    database tables for teams and players

--- db/createDB.py
+++ db/createDB.py
@@ -1,21 +1,42 @@
 import sqlite3
+from scrapeData import getPlayers2
 from scrapeData import getTeams
 
 connect = sqlite3.connect('nbadata.db')
 
 cursor = connect.cursor()
 
-cursor.execute("DROP TABLE IF EXISTS nba_teams")
+# ----- nba teams table -----
 
-teams_table = """ CREATE TABLE nba_teams (
+# cursor.execute("DROP TABLE IF EXISTS nba_teams")
+# teams_table = """ CREATE TABLE nba_teams (
+#                 id int NOT NULL PRIMARY KEY,
+#                 name varchar(255) NOT NULL,
+#                 api_id int NOT NULL
+#             ); """
+# cursor.execute(teams_table)
+# add_teams = "INSERT INTO nba_teams VALUES (?, ?, ?)"
+# cursor.executemany(add_teams, getTeams())
+
+
+# ----- nba players table -----
+
+cursor.execute("DROP TABLE IF EXISTS nba_players")
+players_table = """ CREATE TABLE nba_players (
                 id int NOT NULL PRIMARY KEY,
-                name varchar(255) NOT NULL
+                name varchar(255) NOT NULL,
+                api_id int NOT NULL,
+                api_team_id int NOT NULL,
+                jersey int
             ); """
+cursor.execute(players_table)
+add_players = "INSERT INTO nba_players VALUES (?, ?, ?, ?, ?)"
+cursor.executemany(add_players, getPlayers2())
 
-cursor.execute(teams_table)
+# q = "SELECT * from nba_players WHERE api_team_id=1610612744"
+# cursor.execute(q)
+# print(cursor.fetchall())
 
-print("Table is Ready!")
-
-# add_teams = """ INSERT INTO nba_teams VALUES (); """
+connect.commit()
 
 connect.close()
--- db/scrapeData.py
+++ db/scrapeData.py
@@ -1,10 +1,39 @@
 from nba_api.stats.static import teams, players
+from nba_api.stats.endpoints import commonteamroster
+import requests
 
 def getTeams():
     all_teams = teams.get_teams()
-    return [i['full_name'] for i in all_teams]
+    toRet = []
+    j = 0
+    for i in all_teams:
+        toRet.append((j, i['full_name'], i['id']))
+        j += 1
+    return toRet
 
-# all_players = players.get_active_players()
+# print(teams.get_teams()[0])
 
-# for i in all_players:
-#     print(i['full_name'])
+def getPlayers():
+    all_players = players.get_active_players()
+    toRet = []
+    j = 1
+    for i in all_players:
+        toRet.append((j, i['full_name'], i['id']))
+        j += 1
+    return toRet
+
+# print(players.get_active_players()[0])
+
+def getPlayers2():
+    toRet = []
+    k = 1
+    for i in getTeams():
+        temp = commonteamroster.CommonTeamRoster(i[2]).get_normalized_dict()
+        for j in temp['CommonTeamRoster']:
+            toRet.append((k, j['PLAYER'], j['PLAYER_ID'], j['TeamID'], j['NUM']))
+            k += 1
+    return toRet
+
+# temp = commonteamroster.CommonTeamRoster(1610612737).get_normalized_dict()
+# for i in temp['CommonTeamRoster']:
+#     print(i)
